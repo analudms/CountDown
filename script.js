70aeda069756579ce19e0528f60bfcc0d001c108
analudms <analuizamelo3m@gmail.com>
2023-11-03 11:45:18 -0300

    creating function to count time

---
+++ script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const formatDigit = (digit) => `0${digit}`.slice(-2)
+
+
+const update = (temp) => {
+    const seconds =  document.getElementById('seconds');
+    const minutes =  document.getElementById('minutes');
+    const hours =  document.getElementById('hours');
+    const days =  document.getElementById('days');
+
+
+    const numberSeconds = temp % 60;
+    const numberMinutes = Math.floor(temp % (60 * 60) / 60);
+    const numberHours = Math.floor(temp % (60 * 60 * 24) / (60 * 60));
+    const numberDays = Math.floor(temp / (60 * 60 * 24));
+
+    seconds.textContent = formatDigit(numberSeconds);
+    minutes.textContent = formatDigit(numberMinutes);
+    hours.textContent = formatDigit(numberHours);
+    days.textContent = formatDigit(numberDays);
+}
+
+
+
+const countDown = (temp) => {
+    const stopCount = () => clearInterval(id);
+
+    const count = () => {
+        if (temp === 0){
+            stopCount();
+        }
+        update(temp);
+        temp --;
+    }
+    const id = setInterval(count, 1000);
+}
+
+
+countDown(1120000);
